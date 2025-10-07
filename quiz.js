b5c51223d27e75dfb7b47caca72c9f1f3ea59619
Ephrata <ephrata0327@gmail.com>
2025-10-07 11:06:02 +0300

    edit some lines

--- quiz.js
+++ quiz.js
@@ -1,15 +1,16 @@
 const correctAnswer = "4";
 const feedback = document.getElementById("feedback");
 
+
 function checkAnswer() {
-    const userAnswer = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
     if (!userAnswer) {
         feedback.textContent = "Please select an answer.";
         return;
     }
 
-    if (userAnswer.value === correctAnswer) {
+    if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
     } else {
         feedback.textContent = "That's incorrect. Try again!";
